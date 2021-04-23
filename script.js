2c64f78f20758569896e9d3b5aeaf0186e04d458
George <georgezhu211@gmail.com>
2021-04-22 21:08:21 -0500

    add weather api

--- script.js
+++ script.js
@@ -15,4 +15,11 @@ generateGIF()
 
 const weatherKey = 'b144913c37a5db76946c5657770db654'
 
+function generateWeather() {
+  const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${weatherKey}`
+  fetch(weatherAPI, {mode: 'cors'})
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+}
 
+generateWeather()
