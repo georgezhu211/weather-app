0d3a84cb5a23077d99993ca4d55ab1de1d70deb6
George <georgezhu211@gmail.com>
2021-04-22 20:08:37 -0500

    add gif api

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const gifKey = 'hkNe43aiAFBXWFguJw0rG1E4uxBvr36b'
+const img = document.querySelector('img')
+const button = document.querySelector('button')
+
+button.addEventListener('click', generateGIF)
+
+function generateGIF() {
+  const gifAPI = `https://api.giphy.com/v1/gifs/translate?api_key=${gifKey}&s=cats`
+  fetch(gifAPI, {mode: 'cors'})
+    .then((response) => response.json())
+    .then((response) => img.src = response.data.images.original.url)
+}
+
+generateGIF()
+
+const weatherKey = 'b144913c37a5db76946c5657770db654'
+
+
