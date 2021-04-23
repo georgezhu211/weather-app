be80a8b3f24482ce28aac43ac2ac1717e09ff1a0
George <georgezhu211@gmail.com>
2021-04-23 01:26:25 -0500

    add city database

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// GIPHY API
 const gifKey = 'hkNe43aiAFBXWFguJw0rG1E4uxBvr36b'
 const img = document.querySelector('img')
-const button = document.querySelector('button')
+const gifButton = document.querySelector('.gif-button')
 
-button.addEventListener('click', generateGIF)
+gifButton.addEventListener('click', generateGIF)
 
 function generateGIF() {
   const gifAPI = `https://api.giphy.com/v1/gifs/translate?api_key=${gifKey}&s=cats`
@@ -13,13 +14,53 @@ function generateGIF() {
 
 generateGIF()
 
+// WEATHER API
+
 const weatherKey = 'b144913c37a5db76946c5657770db654'
 
-function generateWeather() {
-  const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${weatherKey}`
+function generateWeather(cityName) {
+  const weatherAPI = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${weatherKey}`
   fetch(weatherAPI, {mode: 'cors'})
     .then((response) => response.json())
     .then((response) => console.log(response))
 }
 
-generateWeather()
+generateWeather('Japan')
+
+// CITY LIST API
+const input = document.querySelector('input')
+const cityButton = document.querySelector('.city-button')
+
+cityButton.addEventListener('click', (e) => {
+  e.preventDefault()
+  const cityName = formatInput(input.value)
+  searchCity(cityName)
+})
+
+function searchCity(cityName) {
+  fetch('./city.list.json')
+    .then((response) => response.json())
+    .then((response) => process(response, cityName))
+    .then((cities) => {
+      cities.forEach((city) => {
+        generateWeather(city.name)
+      })
+    })
+    .catch(() => console.log('could not find city'))
+
+}
+
+function formatInput(cityName) {
+  return cityName.charAt(0).toUpperCase() + cityName.slice(1)
+}
+
+function process(cityList, cityName) {
+  cities = []
+  cityList.forEach((city) => {
+    if(city.name == cityName) {
+      cities.push(city)
+    }
+  })
+  return cities
+}
+
